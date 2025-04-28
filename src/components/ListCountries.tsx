67bc4db58c33093b0fc6a28add2d4e623e1cdc7e
Jose Mejia <jmejia.jm449@gmail.com>
2025-04-28 07:18:27 -0500

    test: Test 6

--- src/components/ListCountries.tsx
+++ src/components/ListCountries.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 import { Card,CardContent,CardHeader,CardTitle} from "./ui/Card"
 import ErrorCountries from "./ErrorComponent";
@@ -9,6 +9,7 @@ import { ApiGetCountries, getClass } from "../services/api/ApiServices";
 /** Componente que consulta la información de los paises, filtra y renderiza **/
 export function ListCountries(filters : IFormPropsUnique)
 {
+    const navigate = useNavigate();
     /**Se realiza la consulta para obtener el listado de los paises **/
     const { loading, error, data } = ApiGetCountries(filters);
 
@@ -23,25 +24,27 @@ export function ListCountries(filters : IFormPropsUnique)
     return (
         <>
             <div className={getClassType}>
-                {data?.countries.map((country) => (
-                    
-                    <Card key={country.code} title={`Ver detalle de ${country.name}`}>
-                        <Link to={`/country/${country.code}`}>
-                            <CardHeader>
-                            <CardTitle>{country.name}</CardTitle>
-                            </CardHeader>
-                            <CardContent>
-                            <div className="">
-                                <div className="text-4xl mb-2">{country.emoji}</div>
-                                <p>Capital: {country.capital || 'N/A'}</p>
-                                <span className="mt-2 inline-flex items-center color-text hover:underline">
-                                <IconComponent size={20} className="mr-2" /> Ver detalle
-                                </span>
-                            </div>
-                            </CardContent>
-                        </Link>
-                    </Card>
-                ))}     
+            {data?.countries.map((country) => (
+                <Card
+                key={country.code}
+                title={`Ver detalle de ${country.name}`}
+                className="cursor-pointer"
+                onClick={() => navigate(`/country/${country.code}`)}
+                >
+                <CardHeader>
+                    <CardTitle>{country.name}</CardTitle>
+                </CardHeader>
+                <CardContent>
+                    <div className="">
+                    <div className="text-4xl mb-2">{country.emoji}</div>
+                    <p>Capital: {country.capital || 'N/A'}</p>
+                    <span className="mt-2 inline-flex items-center color-text hover:underline">
+                        <IconComponent size={20} className="mr-2" /> Ver detalle
+                    </span>
+                    </div>
+                </CardContent>
+                </Card>
+            ))}
             </div>
         </>
     );
