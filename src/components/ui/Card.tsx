85f493dd84c44c1f04bc70b07ae4d8138590b03f
Jose Mejia <jmejia.jm449@gmail.com>
2025-04-28 06:43:52 -0500

    fix: Se agrega React.Fragment, al componente Card para corregir el error al momento de cargar el detalle de un país.

--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -4,19 +4,22 @@ import { cn } from "../../lib/utils"
 
 function Card({ className, ...props }: React.ComponentProps<"div">) {
   return (
-    <div
-      data-slot="card"
-      className={cn(
-        "bg-card text-card-foreground flex flex-col gap-6 rounded-xl border py-6 shadow-sm",
-        className
-      )}
-      {...props}
-    />
+    <React.Fragment>
+      <div
+        data-slot="card"
+        className={cn(
+          "bg-card text-card-foreground flex flex-col gap-6 rounded-xl border py-6 shadow-sm",
+          className
+        )}
+        {...props}
+      />
+    </React.Fragment> 
   )
 }
 
 function CardHeader({ className, ...props }: React.ComponentProps<"div">) {
   return (
+    <React.Fragment>
     <div
       data-slot="card-header"
       className={cn(
@@ -25,59 +28,70 @@ function CardHeader({ className, ...props }: React.ComponentProps<"div">) {
       )}
       {...props}
     />
+    </React.Fragment>
   )
 }
 
 function CardTitle({ className, ...props }: React.ComponentProps<"div">) {
   return (
-    <div
-      data-slot="card-title"
-      className={cn("leading-none font-semibold", className)}
-      {...props}
-    />
+    <React.Fragment>
+      <div
+        data-slot="card-title"
+        className={cn("leading-none font-semibold", className)}
+        {...props}
+      />
+    </React.Fragment>
   )
 }
 
 function CardDescription({ className, ...props }: React.ComponentProps<"div">) {
   return (
-    <div
-      data-slot="card-description"
-      className={cn("text-muted-foreground text-sm", className)}
-      {...props}
-    />
+    <React.Fragment>
+      <div
+        data-slot="card-description"
+        className={cn("text-muted-foreground text-sm", className)}
+        {...props}
+      />
+    </React.Fragment>
   )
 }
 
 function CardAction({ className, ...props }: React.ComponentProps<"div">) {
   return (
-    <div
-      data-slot="card-action"
-      className={cn(
-        "col-start-2 row-span-2 row-start-1 self-start justify-self-end",
-        className
-      )}
-      {...props}
-    />
+    <React.Fragment>
+      <div
+        data-slot="card-action"
+        className={cn(
+          "col-start-2 row-span-2 row-start-1 self-start justify-self-end",
+          className
+        )}
+        {...props}
+      />
+    </React.Fragment>
   )
 }
 
 function CardContent({ className, ...props }: React.ComponentProps<"div">) {
   return (
-    <div
-      data-slot="card-content"
-      className={cn("px-6", className)}
-      {...props}
-    />
+    <React.Fragment>
+      <div
+        data-slot="card-content"
+        className={cn("px-6", className)}
+        {...props}
+      />
+    </React.Fragment>
   )
 }
 
 function CardFooter({ className, ...props }: React.ComponentProps<"div">) {
   return (
-    <div
-      data-slot="card-footer"
-      className={cn("flex items-center px-6 [.border-t]:pt-6", className)}
-      {...props}
-    />
+    <React.Fragment>
+      <div
+        data-slot="card-footer"
+        className={cn("flex items-center px-6 [.border-t]:pt-6", className)}
+        {...props}
+      />
+    </React.Fragment>
   )
 }
 
