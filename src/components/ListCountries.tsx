36113ef80d20c4a5710412292b207163cbca663a
Jose Mejia <jmejia.jm449@gmail.com>
2025-04-28 07:20:13 -0500

    test: Test 7

--- src/components/ListCountries.tsx
+++ src/components/ListCountries.tsx
@@ -1,4 +1,4 @@
-import { Link, useNavigate } from "react-router-dom";
+import {useNavigate } from "react-router-dom";
 
 import { Card,CardContent,CardHeader,CardTitle} from "./ui/Card"
 import ErrorCountries from "./ErrorComponent";
